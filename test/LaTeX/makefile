df9bcbffd58341ac4c649ebaf0ed55a8a9354394
patreggo <tarasovartem7@gmail.com>
2023-10-10 23:15:46 +0300

    тех задание

---
+++ test/LaTeX/makefile
@@ -0,0 +1,6 @@
+src=main.tex setup.tex Введение.tex Реферат.tex ТехЗадание.tex Анализ.tex ТехПроект.tex ТитульныйЛист.tex ТитульныйЛистПрактика.tex Обозначения.tex РабочийПроект.tex Заключение.tex СписокИсточников.tex ЛистЗадания.tex Содержание.tex Плакаты.tex Код.tex vkr.cls
+
+all: main.pdf
+
+main.pdf: $(src)
+	xelatex $(src)
--- test/server.py
+++ test/server.py
@@ -1,7 +1,7 @@
 from waitress import serve
 
 def render_template(template_name='index.html', context={}):
-    
+
     html_str=""
     with open(template_name, 'r') as f:
         html_str=f.read()
@@ -14,12 +14,17 @@ def home(environ):
 def contact_us(environ):
     return render_template(template_name='contact.html', context={})
 
+def contact2(environ):
+    return render_template(template_name='2.html', context={})
+
 def app(environ, start_response):
     path= environ.get("PATH_INFO")
     if path == "/":
         data = home(environ)
     elif path == "/contact":
         data = contact_us(environ)
+    elif path == "/contact/2":
+        data = contact2(environ)
     else:
         data = render_template(template_name='404.html', context={"path":path})
     data = data.encode("utf-8")
